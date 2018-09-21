12c0a252a7c4c23db2383ab9226433cd413f1396
01101010 <jander43@vols.utk.edu>
2018-09-21 13:21:00 -0400

    Updated data structure for Investing class, began implementing gen_statement func, and added more TODO items

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Just a simple collection of "convenience" scripts made to be used from the comma
 * 1: Implement CLI for `investing.py`
 * 2: Implement clean and formatted printing for `investing.py`
 * 3: Implement meta-programming in _statment_ of Investing class
+* 4: Implement `_gen_column_padding` function
--- investing.py
+++ investing.py
@@ -18,22 +18,35 @@ class Investing(object):
     expenses (NOTE: Dividends are reinvested)
     """
     def __init__(self):
-        self._years = 30
-        self._funds = 10000.0
-        self._apr = 0.04
-        self._epr = 0.0011
-        self._expense = 0.0
-        self._netdiv = 0.0
-        self._statement = 'Year {0:>3} | total funds: {1:>12.0f} | total dividends: {2:>6.0f} | current dividends {3:>6.0f}, monthly dividends:{4}  total expense: {5}'
+        self.data = {'years': 30, 'funds': 10000.0, 'apr': 0.04,
+                     'epr': 0.0011
+                     }
+        self._statement = self._gen_statement()
+
+    def _gen_column_padding(self):
+        # TODO
+        pass
+
+    def _gen_statement(self):
+        # TODO
+        statement = ('Year {0} | total funds: {1} | total '
+                     'dividends: {2} | current dividends {3}, '
+                     'monthly dividends:{4}  total expense: {5}'
+                     ).format('{0:>3}', '{1:>12.0f}', '{2:>6.0f}',
+                              '{3:>6.0f}', '{4:>6.0f}', '{5:>6.0f}'
+                              )
+
+        return statement
 
     def calculate(self):
         # first shorten variables for temporary reasons
-        years = self._years
-        funds = self._funds
-        apr = self._apr
-        epr = self._epr
-        expense = self._expense
-        netdiv = self._netdiv
+        data = self.data
+        years = data['years']
+        funds = data['funds']
+        apr = data['apr']
+        epr = data['epr']
+        expense = 0.0
+        netdiv = 0.0
 
         # now calculate
         for i in range(years):
